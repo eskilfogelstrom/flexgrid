8601684fbe454c8cb09edb1a81553d73d269f63a
Eskil Fogelström <eskil@eskilfogelstrom.com>
2016-10-15 17:04:52 +0200

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var rename = require('gulp-rename');
+var cssnano = require('gulp-cssnano');
+
+gulp.task('build', function() {
+  return gulp.src('src/flexgrid.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 3 versions']
+    }))
+    .pipe(gulp.dest('dist'))
+});
+
+gulp.task('build:min', ['build'], function() {
+  return gulp.src('dist/flexgrid.css')
+    .pipe(cssnano())
+    .pipe(rename(function(path) {
+      path.basename += '.min';
+    }))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', ['build'], function() {
+  gulp.watch('src/**/*.scss', function() {
+    gulp.run('build');
+  });
+});
